05862433addb0397d8eacd22194275f473cd3004
YXL <liyunxin2006@163.com>
2016-10-22 21:30:37 +0800

    Update ONHomeViewController

--- OrangeNet/Class/ONHomeViewController.swift
+++ OrangeNet/Class/ONHomeViewController.swift
@@ -37,6 +37,10 @@ class ONHomeViewController: UIViewController {
         action()
     }
     
+    public enum ONError: Error {
+        case Example(String)
+    }
+    
     func testReactiveCocoa() {
         /*scopedExample("Subscription") {
             // Signal.pipe is a way to manually control a signal. the returned observer can be used to send values to the signal
@@ -260,14 +264,364 @@ class ONHomeViewController: UIViewController {
             }
         }*/
         
-        scopedExample("`startWithInterrupted`") {
+        /*scopedExample("`startWithInterrupted`") {
             let disposable = SignalProducer<Int, NoError>.never
                 .startWithInterrupted {
                     print("interrupted called")
             }
             
             disposable.dispose()
-        }
+        }*/
+        
+        /*scopedExample("`lift`") {
+            var counter = 0
+            let transform: (Signal<Int, NoError>) -> Signal<Int, NoError> = { signal in
+                counter = 42
+                return signal
+            }
+            
+            SignalProducer<Int, NoError>(value: 0)
+                .lift(transform)
+                .startWithValues { _ in
+                    print(counter)
+            }
+            
+        }*/
+        
+        /*scopedExample("`map`") {
+            SignalProducer<Int, NoError>(value: 1)
+                .map { $0 + 41 }
+                .startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("`mapError`") {
+            SignalProducer<Int, NSError>(error: NSError(domain: "mapError", code: 42, userInfo: nil))
+                .mapError { ONError.Example($0.description) }
+                .startWithFailed { error in
+                    print(error)
+            }
+        }*/
+        
+        /*scopedExample("`filter`") {
+            SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+                .filter { $0 > 3}
+                .startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("`take`") {
+            SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+                .take(first: 2)
+                .startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("`observeOn`") {
+            let baseProducer = SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+            let completion = { print("is main thread? \(Thread.current.isMainThread)") }
+            
+            if #available(OSX 10.10, *) {
+                baseProducer
+                    .observe(on: QueueScheduler(qos: DispatchQoS.default, name: "test"))
+                    .startWithCompleted(completion)
+            }
+            
+            baseProducer
+                .startWithCompleted(completion)
+        }*/
+        
+        /*scopedExample("'collect()'") {
+            SignalProducer<Int, NoError> { observer, disposable in
+                observer.send(value: 1)
+                observer.send(value: 2)
+                observer.send(value: 3)
+                observer.send(value: 4)
+                observer.sendCompleted()
+            }
+            .collect()
+            .startWithValues { value in
+                print(value)
+            }
+        }*/
+        
+        /*scopedExample("'collect(count:)'") {
+            SignalProducer<Int, NoError> { observer, disposable in
+                observer.send(value: 1)
+                observer.send(value: 2)
+                observer.send(value: 3)
+                observer.send(value: 4)
+                observer.send(value: 5)
+            }
+            .collect(count: 2)
+            .startWithValues { value in
+                        print(value)
+            }
+        }*/
+        
+        /*scopedExample("`collect(predicate:)` matching values inclusively") {
+            SignalProducer<Int, NoError> { observer, disposable in
+                observer.send(value: 1)
+                observer.send(value: 1)
+                observer.send(value: 1)
+                observer.send(value: 2)
+                observer.send(value: 3)
+                observer.send(value: 4)
+                observer.sendCompleted()
+                }
+//                .collect { values in values.reduce(0, +) == 3 }
+                .collect { values in values.reduce(0, { a, b in a + b }) == 3 }
+                .startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("`collect(predicate:)` matching values exclusively") {
+            SignalProducer<Int, NoError> { observer, disposable in
+                observer.send(value: 1)
+                observer.send(value: 2)
+                observer.send(value: 3)
+                observer.send(value: 4)
+                observer.send(value: 5)
+                observer.sendCompleted()
+                }
+                .collect { values, next in next == 3 || next == 5 }
+                .startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("`combineLatestWith`") {
+            let producer1 = SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+            let producer2 = SignalProducer<Int, NoError>(values: [ 1, 2 ])
+            
+            producer1
+                .combineLatest(with: producer2)
+                .startWithValues { value in
+                    print("\(value)")
+            }
+        }*/
+        
+        /*scopedExample("`skip`") {
+            let producer1 = SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+            
+            producer1
+                .skip(first: 2)
+                .startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("`materialize`") {
+            SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+                .materialize()
+                .startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("'sampleOn'") {
+            let baseProducer = SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+            let sampledOnProducer = SignalProducer<Int, NoError>(values: [ 1, 2 ]).map{ _ in () }
+            
+            let newProduce = baseProducer.sample(on: sampledOnProducer)
+            
+            newProduce.startWithValues { value in
+                print(value)
+            }
+        }*/
+        
+        /*scopedExample("`combinePrevious`") {
+            SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+                .combinePrevious(42)
+                .startWithValues { value in
+                    print("\(value)")
+            }
+        }*/
+        
+        /*scopedExample("`scan`") {
+            SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+                .scan(0, +)
+                .startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("`reduce`") {
+            SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+                .reduce(0, +)
+                .startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("`skipWhile`") {
+            SignalProducer<Int, NoError>(values: [ 3, 3, 3, 3, 1, 2, 3, 4 ])
+//                .skipRepeats({ (a, b) -> Bool in
+//                    print("a:\(a)----b:\(b)")
+//                    return a != b
+//                })
+                .skipRepeats { a, b in a != b }
+//                .skipRepeats { _, _ in true }
+                .startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("`skipWhile`") {
+            SignalProducer<Int, NoError>(values: [ 3, 3, 3, 3, 1, 2, 3, 4 ])
+                .skip(while: { $0 > 2 })
+                .startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("`takeUntilReplacement`") {
+            let (replacementSignal, incomingReplacementObserver) = Signal<Int, NoError>.pipe()
+            
+            let baseProducer = SignalProducer<Int, NoError> { incomingObserver, _ in
+                incomingObserver.send(value: 1)
+                incomingObserver.send(value: 2)
+                incomingObserver.send(value: 3)
+                // 下面被替换的信号生成器发送了事件，之后就不再发送baseProducer的事件了
+                // 相当于被替换了
+                incomingReplacementObserver.send(value: 42)
+                
+                incomingObserver.send(value: 4)
+                
+                incomingReplacementObserver.send(value: 42)
+            }
+            
+            let producer = baseProducer.take(untilReplacement: replacementSignal)
+            
+            producer.startWithValues { value in
+                print(value)
+            }
+        }*/
+        
+        /*scopedExample("`takeLast`") {
+            SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+                .take(last: 2)
+                .producer.startWithValues { value in
+                    print(value)
+            }
+        }*/
+
+        /*scopedExample("`ignoreNil`") {
+            SignalProducer<Int?, NoError>(values: [ nil, 1, 2, nil, 3, 4, nil ])
+                .skipNil()
+                .producer.startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("`zipWith`") {
+            let baseProducer = SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+            let zippedProducer = SignalProducer<Int, NoError>(values: [ 42, 43 ])
+            
+            baseProducer
+                .zip(with: zippedProducer)
+                .producer.startWithValues { value in
+                    print("\(value)")
+            }
+        }*/
+        
+        /*scopedExample("`times`") {
+            var counter = 0
+            
+            SignalProducer<(), NoError> { observer, disposable in
+                counter += 1
+                observer.sendCompleted()
+                }
+                .times(42)
+                .start()
+            
+            print(counter)
+        }*/
+        
+        /*scopedExample("`retry`") {
+            var tries = 0
+            
+            SignalProducer<Int, NSError> { observer, disposable in
+                    if tries == 0 {
+                        tries += 1
+                        observer.send(error: NSError(domain: "retry", code: 0, userInfo: nil))
+                    } else {
+                        observer.send(value: 42)
+                        observer.sendCompleted()
+                    }
+                }
+                .retry(upTo: 1)
+                .startWithResult { result in
+                    print(result)
+                }
+        }*/
+
+        /*scopedExample("`then`") {
+            let baseProducer = SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+            let thenProducer = SignalProducer<Int, NoError>(value: 42)
+            
+            baseProducer
+                .then(thenProducer)
+                .producer.startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("`replayLazily`") {
+            let baseProducer = SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4, 42 ])
+                .replayLazily(upTo: 2)
+            
+            baseProducer.startWithValues { value in
+                print(value)
+            }
+            
+            baseProducer.startWithValues { value in
+                print(value)
+            }
+            
+            baseProducer.startWithValues { value in
+                print(value)
+            }
+        }*/
+        
+        /*scopedExample("`flatMap(.Latest)`") {
+            SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+                .flatMap(.latest, transform: { SignalProducer(value: $0 + 3) })
+                .startWithValues { value in
+                    print(value)
+                }
+        }*/
+        
+        /*scopedExample("`flatMapError`") {
+            SignalProducer<Int, NSError>(error: NSError(domain: "flatMapError", code: 42, userInfo: nil))
+                .flatMapError { SignalProducer<Int, NoError>(value: $0.code) }
+                .startWithValues { value in
+                    print(value)
+            }
+        }*/
+        
+        /*scopedExample("`sampleWith`") {
+            let producer = SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4 ])
+            let sampler = SignalProducer<String, NoError>(values: [ "a", "b" ])
+            
+            let result = producer.sample(with: sampler)
+
+            result.startWithValues { left, right in
+                print("\(left) \(right)")
+            }
+        }*/
+        
+        /*scopedExample("`log events`") {
+            let baseProducer = SignalProducer<Int, NoError>(values: [ 1, 2, 3, 4, 42 ])
+            
+            baseProducer
+                .logEvents(identifier: "Playground is fun!")
+                .start()
+        }*/
         
     }
 
